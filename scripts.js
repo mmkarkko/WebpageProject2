051d04798403c93ffe2e797dad4aa44898deff90
mmkarkko <miia.arkko@gmail.com>
2024-07-24 17:21:31 +0300

    Email and CV buttons added

--- scripts.js
+++ scripts.js
@@ -31,3 +31,20 @@ document.addEventListener("DOMContentLoaded", function () {
     console.error("One or more elements not found");
   }
 });
+
+/* Creates contact-buttons */
+document.addEventListener("DOMContentLoaded", function () {
+  var part1 = "contact";
+  var part2 = Math.pow(2, 6);
+  var part3 = String.fromCharCode(part2);
+  var part4 = "miiaarkko.fi";
+  var part5 = part1 + String.fromCharCode(part2) + part4;
+
+  var emailLink =
+    "<a href='mailto:" + part5 + "' class='button email-button'>Contact Me</a>";
+  var cvLink =
+    "<a href='path/to/your-cv.pdf' class='button cv-button'>Download CV</a>";
+
+  document.getElementById("contact-buttons").innerHTML =
+    emailLink + " " + cvLink;
+});
