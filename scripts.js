0d58ef717b900c94da34c400e31518e496888a8e
mmkarkko <miia.arkko@gmail.com>
2024-08-07 10:01:44 +0300

    Some aria considerations added

--- scripts.js
+++ scripts.js
@@ -18,12 +18,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.querySelector(".toggle_btn");
   const toggleBtnIcon = document.querySelector(".toggle_btn i");
   const dropDownMenu = document.querySelector(".dropdown_menu");
-
   if (toggleBtn && toggleBtnIcon && dropDownMenu) {
     toggleBtn.onclick = function () {
       dropDownMenu.classList.toggle("open");
       const isOpen = dropDownMenu.classList.contains("open");
-
       toggleBtnIcon.classList = isOpen
         ? "fa-solid fa-xmark"
         : "fa-solid fa-bars";
